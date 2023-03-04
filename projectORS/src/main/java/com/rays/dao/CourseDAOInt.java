05224bc88cef54d4f81d64a702daaa65f2f2d424
sapnajariya <jariyasapna@gmail.com>
2023-03-03 16:08:45 -0800

    msg

---
+++ projectORS/src/main/java/com/rays/dao/CourseDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CourseDTO;
+import com.rays.dto.StudentDTO;
+/**
+ * @author Sapna Jariya
+ *
+ */
+public interface CourseDAOInt extends BaseDAOInt<CourseDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Sapna Jariya
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
